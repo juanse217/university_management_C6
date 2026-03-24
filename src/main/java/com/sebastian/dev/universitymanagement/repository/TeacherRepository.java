10504117dbca87b7d03e2c03a76319429e64e3c2
juanse217 <juansebas08004@gmail.com>
2026-03-23 21:56:40 -0500

    formatted

--- src/main/java/com/sebastian/dev/universitymanagement/repository/TeacherRepository.java
+++ src/main/java/com/sebastian/dev/universitymanagement/repository/TeacherRepository.java
@@ -7,32 +7,31 @@ import org.springframework.data.jpa.repository.Query;
 
 import com.sebastian.dev.universitymanagement.model.entity.Teacher;
 
-public interface TeacherRepository  extends JpaRepository<Teacher, Long>{
-    //We want to find teachers by dean (teachers -> faculty -> dean)
+public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+        // We want to find teachers by dean (teachers -> faculty -> dean)
 
-    List<Teacher> findByFacultyDeanIdentification(Long deanId);
+        List<Teacher> findByFacultyDeanIdentification(Long deanId);
 
-    @Query("""
-            SELECT t FROM Teacher t
-            JOIN t.faculty f
-            JOIN f.dean d 
-            WHERE d.identification = :deanId
-            """)
-    List<Teacher> findByDeanIdJPQL(Long deanId);
+        @Query("""
+                        SELECT t FROM Teacher t
+                        JOIN t.faculty f
+                        JOIN f.dean d
+                        WHERE d.identification = :deanId
+                        """)
+        List<Teacher> findByDeanIdJPQL(Long deanId);
 
-    @Query("""
-            SELECT t FROM Teacher t
-            WHERE t.faculty.dean.identification = :deanId
-            """)
-    List<Teacher> findByDeanIdSimplified(Long deanId); //no need for joins, JPA navigates automatically the path.
+        @Query("""
+                        SELECT t FROM Teacher t
+                        WHERE t.faculty.dean.identification = :deanId
+                        """)
+        List<Teacher> findByDeanIdSimplified(Long deanId); // no need for joins, JPA navigates automatically the path.
 
-    @Query(value = """
-            SELECT t.* FROM teachers t
-            JOIN faculties f ON (f.faculty_number = t.faculty_id)
-            JOIN deans d ON (d.faculty_number = f.faculty_number)
-            WHERE d.identification = :deanId
-            """, nativeQuery = true)
-    List<Teacher> findByDeanIdSQL(Long deanId);
-
+        @Query(value = """
+                        SELECT t.* FROM teachers t
+                        JOIN faculties f ON (f.faculty_number = t.faculty_id)
+                        JOIN deans d ON (d.faculty_number = f.faculty_number)
+                        WHERE d.identification = :deanId
+                        """, nativeQuery = true)
+        List<Teacher> findByDeanIdSQL(Long deanId);
 
 }
